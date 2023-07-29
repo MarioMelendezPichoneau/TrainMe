4168a227409849ed4d6659df06c401dcc8c5b662
Luis Angel Garcia Louis <ludevgarcialouis@gmail.com>
2023-07-28 20:49:22 -0400

    resolve .env problem

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -24,6 +24,8 @@ export const LoginPage = () => {
 
   return (
     <section className="bg-white h-screen ">
+      {error && <AlertError message={error} />}
+      {success && <AlertSuccess message={success} />}
       <div className="grid grid-cols-1 lg:grid-cols-2 h-full">
         <div className="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
           <div className="absolute inset-0">
